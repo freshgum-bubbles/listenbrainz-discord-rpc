9745b0b5763b1cb36567ec4980d5da2c747f0924
freshgum <85623267+freshgum-bubbles@users.noreply.github.com>
2023-08-09 22:24:03 +0100

    test(*): add service test skeletons

---
+++ src/timer.service.spec.ts
@@ -0,0 +1,11 @@
+describe('TimerService', () => {
+    describe('createDelay', () => {
+        it.skip('should create an Observable that emits after X ms', () => {
+
+        });
+    });
+
+    describe('createInterval', () => {
+        
+    });
+});
