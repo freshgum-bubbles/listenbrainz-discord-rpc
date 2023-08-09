eb32f3c28f6b8dfa5ecbe7c754e9576ed9043034
freshgum <85623267+freshgum-bubbles@users.noreply.github.com>
2023-08-09 23:00:57 +0100

    fix(core): interpolate user_name in NowPlaying requests

--- src/now-playing.service.ts
+++ src/now-playing.service.ts
@@ -1,20 +1,24 @@
 import { Service } from '@freshgum/typedi';
 import { ListenBrainzClientService } from './listenbrainz-client.service.ts';
-import { ListenBrainzEndpoint } from './constants/listenbrainz-endpoints.const.ts';
+import { ListenBrainzEndpoint, ListenBrainzEndpointTemplates } from './constants/listenbrainz-endpoints.const.ts';
 import { ListenBrainzPlayingNowResponse } from './api-responses/NowPlaying.ts';
 import { doesZodTypeMatchValue } from './utilities/zod-guard.util.ts';
 import { LogService } from './log.service.ts';
+import { LISTENBRAINZ_USERNAME } from './constants/listenbrainz-username.const.ts';
 
-@Service([ListenBrainzClientService, LogService])
+@Service([ListenBrainzClientService, LogService, LISTENBRAINZ_USERNAME])
 export class NowPlayingService {
 	constructor(
 		private listenBrainzClient: ListenBrainzClientService,
-        private logger: LogService
+        private logger: LogService,
+        private userName: string
 	) {}
 
 	public async getNowPlayingFromListenBrainz(): Promise<ListenBrainzPlayingNowResponse | null> {
         const endpoint = ListenBrainzEndpoint.UserPlayingNow;
-		const response = await this.listenBrainzClient.fetchListenBrainzEndpoint(endpoint);
+		const response = await this.listenBrainzClient.fetchListenBrainzEndpoint(endpoint, {
+            values: { user_name: this.userName }
+        });
         
         if (response.status !== 200) {
             this.logger.log('ListenBrainz returned a non-200 status code: ', response.status);
