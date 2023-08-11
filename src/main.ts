7954ee0f67cb8ca2542d7c1c36695cf96d7c736c
freshgum <85623267+freshgum-bubbles@users.noreply.github.com>
2023-08-11 17:47:54 +0100

    fix(core): update LB API poll interval

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { LISTENBRAINZ_TOKEN } from "./constants/listenbrainz-token.const.ts";
 import { RootService } from "./root.service.ts";
 import { LogService } from "./log.service.ts";
 import { DISCORD_RPC_TRANSPORT } from "./constants/discord-rpc-transport.const.ts";
+import { addSecretsToContainer } from "./secrets.ts";
 
 /** Create a new container for the app. */
 const appContainer = Container.ofChild(Symbol('listenbrainz-discord'));
@@ -14,8 +15,7 @@ const appContainer = Container.ofChild(Symbol('listenbrainz-discord'));
 appContainer.setValue(DISCORD_RPC_APPLICATION_SCOPES, ['rpc', 'rpc.api', 'messages.read']);
 appContainer.setValue(DISCORD_RPC_TRANSPORT, 'ipc');
 appContainer.setValue(LISTENBRAINZ_HOST, 'https://api.listenbrainz.org/');
-appContainer.setValue(LISTENBRAINZ_POLL_INTERVAL, 15 * 1000); // 15 seconds
-appContainer.setValue(LISTENBRAINZ_POLL_INTERVAL, 2 * 1000); // 15 seconds
+appContainer.setValue(LISTENBRAINZ_POLL_INTERVAL, 5 * 1000); // 15 seconds
 
 addSecretsToContainer(appContainer);
 
