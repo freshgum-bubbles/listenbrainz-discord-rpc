c5a62fb681313e7448d504d59c691c792cb2203c
freshgum <85623267+freshgum-bubbles@users.noreply.github.com>
2023-08-11 17:48:40 +0100

    fix(api): ensure unknown tracks are displayed
    Allow the application to support responses which
    indicate that ListenBrainz could not find an item
    for the track in the MusicBrainz database.
    
    This commit also includes the addition of buttons
    to the Discord presence listing.

--- src/api-responses/NowPlaying.ts
+++ src/api-responses/NowPlaying.ts
@@ -3,15 +3,15 @@ import { z } from "zod";
 // TODO: is TrackMetadata what the API calls this? make sure it is
 export const ListenBrainzTrackMetadata = z.object({
     additional_info: z.object({
-        artist_mbids: z.array(z.string()),
-        release_mbid: z.string(),
+        artist_mbids: z.optional(z.array(z.string())),
+        release_mbid: z.optional(z.string()),
         submission_client: z.string(),
         submission_client_version: z.string(),
-        track_mbid: z.string(),
-        tracknumber: z.number(),
+        track_mbid: z.optional(z.string()),
+        tracknumber: z.optional(z.number()),
     }),
     artist_name: z.string(),
-    release_name: z.string(),
+    release_name: z.optional(z.string()),
     track_name: z.string(),
 });
 
--- src/discord-rpc-presence.service.ts
+++ src/discord-rpc-presence.service.ts
@@ -1,17 +1,117 @@
-import { Service } from "@freshgum/typedi";
-import { Presence } from "discord-rpc";
-import { ListenBrainzTrackMetadata } from "./api-responses/NowPlaying.ts";
+import { Optional, Service } from '@freshgum/typedi';
+import { Presence } from 'discord-rpc';
+import { ListenBrainzTrackMetadata } from './api-responses/NowPlaying.ts';
+import { DiscordRPCPresenceButton } from './types/discord-rpc-presence-button.type.ts';
+import { LISTENBRAINZ_USERNAME } from './constants/listenbrainz-username.const.ts';
 
-@Service([ ])
+@Service([
+    [LISTENBRAINZ_USERNAME, Optional()]
+])
 export class DiscordRPCPresenceService {
-    createPresenceFromTrackMetadata (track: ListenBrainzTrackMetadata) {
-        const { release_name: albumName, track_name: trackName, artist_name: artistName } = track;
-        const presence: Presence = {
-            /** Set the state to the album name. */
-            state: albumName,
-            details: `Listening to ${trackName} - ${artistName}`
-        };
+    constructor (private listenBrainzUsername: string | null) { }
 
-        return presence;
+	public createPresenceFromTrackMetadata(track: ListenBrainzTrackMetadata) {
+		const {
+			release_name: albumName,
+			track_name: trackName,
+			artist_name: artistName,
+		} = track;
+		const {
+			submission_client: clientName,
+			submission_client_version: clientVersion,
+		} = track.additional_info;
+
+		/**
+		 * Gracefully degrade to just saying we don't know who the artist is
+		 * when the track wasn't recognised my MusicBrainz.
+		 */
+		const state = albumName ? `${artistName} - ${albumName}` : 'Unknown Artist - Unknown Album';
+
+		const presence: Presence = {
+			details: `Listening to ${trackName}`,
+			/** Set the state to the album name. */
+			state,
+			smallImageKey: 'now-playing-icon',
+			smallImageText: `Submitted via ${clientName}, version ${clientVersion}`,
+		};
+
+		presence.buttons = this.createButtons(track, presence);
+		return presence;
+	}
+
+	public createButtons(
+		track: ListenBrainzTrackMetadata,
+		presence: Omit<Presence, 'buttons'>
+	): DiscordRPCPresenceButton[] {
+		return [
+            this.createViewMyListenBrainzButton(),
+			this.createViewTrackOnMusicBrainzButton(track),
+			// this.createViewAlbumOnMusicBrainzButton(track)
+		].filter(item => item !== null) as DiscordRPCPresenceButton[];
+	}
+
+    private createViewMyListenBrainzButton(): DiscordRPCPresenceButton | null {
+        if (!this.listenBrainzUsername) {
+            return null;
+        }
+
+        return {
+            label: 'View my ListenBrainz',
+            url: `https://listenbrainz.org/user/${this.listenBrainzUsername}`
+        }
     }
+
+	private createViewTrackOnMusicBrainzButton(
+		track: ListenBrainzTrackMetadata
+	): DiscordRPCPresenceButton | null {
+		const url = this.createMusicBrainzRecordingURLForTrack(track);
+
+		if (!url) {
+			return null;
+		}
+
+		return {
+			label: 'View Track on MusicBrainz',
+			url,
+		};
+	}
+
+	private createMusicBrainzRecordingURLForTrack(
+		track: ListenBrainzTrackMetadata
+	): string | null {
+		const { track_mbid: trackMbID } = track.additional_info;
+
+		if (!trackMbID) {
+			return null;
+		}
+
+		return `https://musicbrainz.org/recording/${trackMbID}`;
+	}
+
+	private createViewAlbumOnMusicBrainzButton(
+		track: ListenBrainzTrackMetadata
+	): DiscordRPCPresenceButton | null {
+		const url = this.createMusicBrainzReleaseURLForTrack(track);
+
+		if (!url) {
+			return null;
+		}
+
+		return {
+			label: 'View Album on MusicBrainz',
+			url,
+		};
+	}
+
+	private createMusicBrainzReleaseURLForTrack(
+		track: ListenBrainzTrackMetadata
+	): string | null {
+		const { release_mbid: releaseMbID } = track.additional_info;
+
+		if (!releaseMbID) {
+			return null;
+		}
+
+		return `https://musicbrainz.org/release/${releaseMbID}`;
+	}
 }
