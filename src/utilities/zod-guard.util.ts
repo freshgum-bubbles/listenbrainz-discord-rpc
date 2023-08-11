a0576f1c8c609a0eecbc8b42ff6f283707be4171
freshgum <85623267+freshgum-bubbles@users.noreply.github.com>
2023-08-11 17:48:11 +0100

    feat(utils): log zod validation errors to console

--- src/utilities/zod-guard.util.ts
+++ src/utilities/zod-guard.util.ts
@@ -1,11 +1,20 @@
+import { Container } from '@freshgum/typedi';
 import zod from 'zod';
+import { LogService } from '../log.service.ts';
 
 export function doesZodTypeMatchValue<T> (zodType: zod.ZodType<T>, input: unknown): input is T {
+    const validationResult = zodType.safeParse(input);
+
+    if (validationResult.success === false) {
+        // TODO: move this into a service
+        Container.get(LogService).log(`Invalid validation result: ${validationResult.error}`);
+    }
+
     /**
      * Between you and me, I'm only using Zod because the runtypes plugin for Insomnia
      * doesn't exist any more.  I do generally prefer runtypes over Zod.
      * 
      * The fact that it doesn't have a type guard built in is a bit shit :/
      */
-    return zodType.safeParse(input).success;
+    return validationResult.success;
 }
